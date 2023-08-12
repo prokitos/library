c6796273b65818d72e1910b3b69d85e15ddb87d7
prokitos <typada@yandex.ru>
2023-08-12 19:10:51 +0300

    gitignore add

--- cmake_gtest/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: G:/projects/cplus/LIBRARY/cmake_gtest
-# Build directory: G:/projects/cplus/LIBRARY/cmake_gtest/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("G:/projects/cplus/LIBRARY/cmake_gtest/build/hello_test[1]_include.cmake")
-subdirs("_deps/googletest-build")
--- cmake_gtest/build/tests[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "G:/projects/cplus/LIBRARY/cmake_gtest/build/tests[1]_tests.cmake")
-  include("G:/projects/cplus/LIBRARY/cmake_gtest/build/tests[1]_tests.cmake")
-else()
-  add_test(tests_NOT_BUILT tests_NOT_BUILT)
-endif()
