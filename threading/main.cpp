621c5a23f77d3b526bcc5d5f2fc7c81078adf225
prokitos <typada@yandex.ru>
2023-09-29 19:47:27 +0300

    multi threading

--- threading/main.cpp
+++ threading/main.cpp
@@ -1,10 +1,10 @@
 #include <vector>
 #include <thread>
+#include <mutex>
 #include <cmath>
 #include <iostream>
 #include <algorithm>
 
-int result = 0;
 void consoleClear()
 {
 
@@ -15,40 +15,96 @@ void consoleClear()
     
 }
 
-int normFibonachi(int input)
+std::mutex mutt;
+
+
+void simple_thread_one()
 {
-    if(input == 0)
-        return 0;
-    else if(input == 1)
-        return 1;
-    else
-        return normFibonachi(input - 1) + normFibonachi(input - 2);
+    for (size_t i = 0; i < 10; i++)
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(300));
+        std::cout << 10 + i << std::endl;
+    }
+}
+void simple_thread_two()
+{
+    for (size_t i = 0; i < 10; i++)
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(300));
+        std::cout << 100 + i << std::endl;
+    }
 }
 
-void thread1(int temp)
+void mutex_fun_one(int x)
 {
-    result += normFibonachi(temp);
+    for (size_t i = 0; i < 10; i++)
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(300));
+
+        mutt.lock();
+        std::cout << x * i << " f1" << std::endl;
+        mutt.unlock();
+    }
 }
 
-void thread2(int temp)
+void mutex_fun_two()
 {
-    result += normFibonachi(temp);
+    
+    for (size_t i = 0; i < 10; i++)
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(300));
+
+        mutt.lock();
+        std::cout << i << " f2" << std::endl;
+        mutt.unlock();
+    }
+    
+}
+
+void thread_ref_fun(int &x)
+{
+    x = x * 5;
+}
+
+int thread_lambda_use(int x)
+{
+    std::this_thread::sleep_for(std::chrono::milliseconds(300));
+    return 100 + x;
 }
 
 int main()
 {
     consoleClear();
 
-    std::thread t1(thread1, 42);
-    std::thread t2(thread2, 42);
+    // использование обычных потоков. + фоновый поток.
+        // std::thread t_name1(simple_thread_one);
+        // t_name1.detach(); // поток фоновый (не важен), и если основной поток закончится то и этот тоже.
+        // std::thread t_name2(simple_thread_two);
+        // t_name2.join();
+    // нужны мьютексы, так как иногда могут быть баги, и плохо печатается в консоль.
 
-    t1.join();
-    t2.join();
 
-    std::cout << result;
+    // использование потоков с мьютексами.
+        //std::thread t_name1(mutex_fun_two);
+        //std::thread t_name2(mutex_fun_one, 5);
+        //t_name2.join();
+        //t_name1.join();
 
-    // с потоками скорость почти в 2 раза быстрее чем без потоков
-    //std::cout << normFibonachi(42) + normFibonachi(42);
+    // использование потоков, и изменение переменной
+        // int a = 10;
+        // std::thread t_name1(thread_ref_fun, std::ref(a));
+        // t_name1.join();
+        // std::cout << a;
+
+
+    // использование лямбда функции для возврата значения из функции в потоке.
+    // int result {};
+    // std::thread lambda([&result]()
+    // {
+    //     result = thread_lambda_use(10);
+    // });
+    // lambda.join();
+    // std::cout << result;
 
     return 0;
 }
