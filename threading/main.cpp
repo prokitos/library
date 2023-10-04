c3e7d56c015abeec85f147034d4028069711bf52
prokitos <typada@yandex.ru>
2023-10-04 23:13:10 +0300

    multithreading
    lock guard and work with class

--- threading/main.cpp
+++ threading/main.cpp
@@ -72,6 +72,42 @@ int thread_lambda_use(int x)
     return 100 + x;
 }
 
+
+class TestClass
+{
+public:
+    void workOne()
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+        std::cout << "work one start" << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+        std::cout << "work one end" << std::endl;
+    }
+    void workTwo()
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+        std::cout << "work two start" << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+        std::cout << "work two end" << std::endl;
+    }
+};
+
+
+void lockGuardTest()
+{
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+
+    // создаем свою область видимости.
+    {
+        std::lock_guard<std::mutex> l_gward(mutt);
+        std::cout << "work thread start" << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+        std::cout << "work thread end" << std::endl;
+    }
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+}
+
+
 int main()
 {
     consoleClear();
@@ -81,7 +117,7 @@ int main()
         // t_name1.detach(); // поток фоновый (не важен), и если основной поток закончится то и этот тоже.
         // std::thread t_name2(simple_thread_two);
         // t_name2.join();
-    // нужны мьютексы, так как иногда могут быть баги, и плохо печатается в консоль.
+        // нужны мьютексы, так как иногда могут быть баги, и плохо печатается в консоль.
 
 
     // использование потоков с мьютексами.
@@ -98,13 +134,28 @@ int main()
 
 
     // использование лямбда функции для возврата значения из функции в потоке.
-    // int result {};
-    // std::thread lambda([&result]()
-    // {
-    //     result = thread_lambda_use(10);
-    // });
-    // lambda.join();
-    // std::cout << result;
+        // int result {};
+        // std::thread lambda([&result]()
+        // {
+        //     result = thread_lambda_use(10);
+        // });
+        // lambda.join();
+        // std::cout << result;
+
+    // выполнение методов класса в потоках.
+        // TestClass instanceTest;
+        // std::thread classLambdaOne([&](){instanceTest.workOne();});
+        // //std::thread classLambdaTwo([&](){instanceTest.workTwo();});   
+        //     // есть другой способо
+        // std::thread classLambdaTwo(&TestClass::workTwo, instanceTest);
+        // classLambdaOne.join();
+        // classLambdaTwo.join();
+
+    // lockGuard в потоках. помощь при использовании мьютекса.
+        std::thread t_name1(lockGuardTest);
+        std::thread t_name2(lockGuardTest);
+        t_name1.join();
+        t_name2.join();
 
     return 0;
 }
